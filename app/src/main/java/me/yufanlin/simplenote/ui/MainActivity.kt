20f500977dc64a2e7a27b21ecd23b4e9d1de121c
phxlin <yfln11@gmail.com>
2021-09-25 13:42:27 -0500

    minor clean up

--- app/src/main/java/me/yufanlin/simplenote/ui/MainActivity.kt
+++ app/src/main/java/me/yufanlin/simplenote/ui/MainActivity.kt
@@ -6,14 +6,17 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import me.yufanlin.simplenote.R
+import me.yufanlin.simplenote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         navController = Navigation.findNavController(this, R.id.fragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
